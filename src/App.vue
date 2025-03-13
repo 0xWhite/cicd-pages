0eed305cc8927cfe3041640073434c1e8aaa27eb
cc <476840998@qq.com>
2025-03-13 11:56:58 +0800

    feat

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Auto deploy, use docker webhook" />
+      <HelloWorld msg="Good Job" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
